d8b2ba19c25eb5d9082baef9fde98f626b690da9
Sanwal Hussain <106494523+sanwalhussain@users.noreply.github.com>
2023-10-23 00:23:40 +0300

    update

--- data/data.ts
+++ data/data.ts
@@ -1,9 +1,9 @@
 const data = {
-    sitename: "ProSafe Arabia",
+    sitename: "ProSafeArabia",
     sitetagline: "Your Safety Solutions Provider in Saudi Arabia",
     sitelogo: "",
     title: "Comming Soon",
-    description: "ProSafe Arabia is dedicated to providing top-notch safety solutions in the heart of Arabia. We're passionately working to create a safer environment for you 📅. Stay tuned as we prepare to unveil our comprehensive range of high-quality safety products and services, meticulously designed for your protection and peace of mind.😍",
+    description: "ProSafeArabia is dedicated to providing top-notch safety solutions in the heart of Arabia. We're passionately working to create a safer environment for you 📅. Stay tuned as we prepare to unveil our comprehensive range of high-quality safety products and services, meticulously designed for your protection and peace of mind.",
     newsletterheading: "Stay tuned for our live updates!",
     copyrightText: 'Copyright © 2023 ProSafeArabia, a division of Waze Dahem Al-Qahtani Trading Est.',
     socialIconsHeading: "Follow Us 📣",
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Home - Comming soon Template",
-  description: 'This is a simple coming soon template built with NextJS and TailwindCSS. It is a lightweight and responsive template that can be used for various projects that require a "coming soon" page.',
+  title: "ProSafeArabia: Your Trusted Safety Solutions Provider",
+  description: 'At ProSafeArabia, your safety is our priority. We are dedicated to delivering top-tier safety solutions tailored to your needs. Our team is working diligently to introduce you to a range of high-quality safety and personal protective equipment. We can not wait to share our latest innovations with you, ensuring your peace of mind in every situation. Stay tuned for a safer tomorrow with ProSafeArabia.',
 }
 
 export default function RootLayout({
